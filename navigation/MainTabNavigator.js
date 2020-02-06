e45ad8ee617eb44c2f69803cbe491754a5f3a8f6
Mehak <mehakvij1@gmail.com>
2020-02-06 12:58:28 -0500

    adding test buttons

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,6 +3,7 @@ import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
